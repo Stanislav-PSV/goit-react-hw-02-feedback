bcf107a122d828f043a1e94a6cf05209558ef79c
Stanislav-PSV <stanislav.psv521@gmail.com>
2022-09-04 21:42:21 +0300

    002

--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,6 +1,8 @@
 import styled from "styled-components";
 
 export const AppContainer = styled.div`
+	background-image: linear-gradient(to top,
+	#4169E1 50%, #FFFF00 50%);
 	background-repeat: no-repeat;
 	background-size: cover;
 	box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.25);
@@ -9,7 +11,6 @@ export const AppContainer = styled.div`
 	height: 100vh;
 
 	justify-content: center;
-	/* flex-direction: column, */
 	align-items: center;
 	font-size: 40px;
 	color: #010101;
