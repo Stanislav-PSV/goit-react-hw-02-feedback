6f30c0185b4b7f3bcd0489a1397a278a412f15c0
Stanislav-PSV <stanislav.psv521@gmail.com>
2022-09-04 21:11:30 +0300

    005

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,92 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Section from './Section';
+import FeedbackOptions from './FeedbackOptions';
+import Statistics from './Statistics';
+import Notification from './Notification';
+import { AppContainer } from './App.styled';
+
+export class App extends Component {
+	static propTypes = {};
+
+	state = {
+		good: 0,
+		neutral: 0,
+		bad: 0,
+	};
+
+	handleInkremet = event => {
+		const { name } = event.currentTarget;
+
+		this.setState(prevState => ({
+			[name]: prevState[name] + 1,
+		}));
+		this.totalFeedback();
+		this.percentageGoodFeedback();
+	};
+
+	hundleInkrementGood = () => {
+		this.setState(prevState => {
+			return {
+				good: prevState.good + 1,
+			};
+		});
+		this.totalFeedback();
+		this.percentageGoodFeedback();
+	};
+
+	hundleInkrementNeutral = () => {
+		this.setState(prevState => {
+			return {
+				neutral: prevState.neutral + 1,
+			};
+		});
+		this.totalFeedback();
+		this.percentageGoodFeedback();
+	};
+
+	hundleInkrementBad = () => {
+		this.setState(prevState => {
+			return {
+				bad: prevState.bad + 1,
+			};
+		});
+		this.totalFeedback();
+		this.percentageGoodFeedback();
+	};
+
+	totalFeedback = () => {
+		const { good, neutral, bad } = this.state;
+		return good + neutral + bad;
+	};
+
+	percentageGoodFeedback = () => {
+		return parseInt((this.state.good / this.totalFeedback()) * 100);
+	};
+	render() {
+		const { good, neutral, bad } = this.state;
+		return (
+			<AppContainer>
+				<Section title="Please leave your feedback">
+					<FeedbackOptions
+						options={['good', 'neutral', 'bad']}
+						onLeaveFeedback={this.handleInkremet}
+					/>
+				</Section>
+
+				<Section title="Statistics">
+					{good !== 0 || neutral !== 0 || bad !== 0 ? (
+						<Statistics
+							good={good}
+							neutral={neutral}
+							bad={bad}
+							total={this.totalFeedback()}
+							positivePercentage={this.percentageGoodFeedback()}
+						/>
+					) : (
+						<Notification notice='There are no reviews yet 🙄 ' />
+					)}
+				</Section>
+			</AppContainer>
+		);
+	}
+}
